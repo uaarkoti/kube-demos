67e8def273bfc25cc275d40ffbcc63012e2d79f1
Tim Hockin <thockin@google.com>
2016-02-10 16:51:40 -0800

    use kubectl apply

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ run "kubectl get namespaces"
 
 desc "Create a namespace for these demos"
 run "cat $(relative demo-namespace.yaml)"
-run "kubectl create -f $(relative demo-namespace.yaml)"
+run "kubectl apply -f $(relative demo-namespace.yaml)"
 
 desc "Hey look, a namespace!"
 run "kubectl get namespaces"
